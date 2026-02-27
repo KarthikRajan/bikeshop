2acbadad0351920a367c8968b8a9616f7f2994cd
Karthik Rajan <karthikrajan@192.168.1.5>
2026-02-27 09:54:36 +0530

    Initial commit - Bike Shop

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🚴 Welcome to Karthik's Bike Shop</h1>
+    <p>Mountain Bike - ₹25,000</p>
+    <p>Road Bike - ₹30,000</p>
+    <p>Electric Bike - ₹60,000</p>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Bike shop running on port 3000");
+});
