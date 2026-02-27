34e79f5861efc8d75f6a6dfa6b7c4fee468fb4c5
karthikrajan <vpkarthikrajan@gmail.com>
2026-02-27 20:57:34 +0530

    Added GitHub Actions CI pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>🚴 Welcome to Karthik's Bike Shop</h1>
+    <h1>🚴 Welcome to Karr's Bike Shop</h1>
     <p>Mountain Bike - ₹25,000</p>
     <p>Road Bike - ₹30,000</p>
     <p>Electric Bike - ₹60,000</p>
